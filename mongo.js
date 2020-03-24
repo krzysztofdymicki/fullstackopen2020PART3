16062b3a64348e8d42eb85eeab1a52551cb93440
Krzychu <krzysztof.dymicki.bp@gmail.com>
2020-03-24 16:07:00 +0100

    Part 3

--- mongo.js
+++ mongo.js
@@ -1,41 +1,41 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log("You have to give a password")
-    process.exit(1)
+  console.log('You have to give a password')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = 
+const url =
  `mongodb+srv://alberteinstein:${password}@fullstackopen-dkb2y.mongodb.net/test?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if(process.argv.length === 3) {
-    Person
+  Person
     .find({})
     .then(persons => {
-        persons.map(person => console.log(person.name, person.number))
-        mongoose.connection.close()
-        process.exit(1)
+      persons.map(person => console.log(person.name, person.number))
+      mongoose.connection.close()
+      process.exit(1)
     })
 }
 
 const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
+  name: process.argv[3],
+  number: process.argv[4]
 })
 
-person.save().then(response => {
-    console.log('person saved')
-    mongoose.connection.close()
+person.save().then(() => {
+  console.log('person saved')
+  mongoose.connection.close()
 })
